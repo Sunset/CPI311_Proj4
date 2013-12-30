88bdaefd93c30bb91858fe8c3a5920128dde6527
Allen Hsia <ahsia1@asu.edu>
2013-12-29 20:49:57 -0700

    initial commit

---
+++ Common/ButtonGroup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Common
+{
+    public class ButtonGroup : UIElement
+    {
+        public int Current { get; set; }
+        KeyboardState prevState = Keyboard.GetState();
+
+        public ButtonGroup(Texture2D background = null,
+            Rectangle? bounds = null)
+            : base(background, bounds)
+        {
+            Current = 0;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+            if(keyboardState.IsKeyDown(Keys.Down)&&prevState.IsKeyUp(Keys.Down))
+                Current = (Current + 1) % Children.Count;
+            if (keyboardState.IsKeyDown(Keys.Up) && prevState.IsKeyUp(Keys.Up))
+                Current = (Current-1+Children.Count) % Children.Count;
+            if (keyboardState.IsKeyDown(Keys.Enter))
+                Children[Current].Click();
+            foreach (UIElement element in Children)
+                element.Color = Color.Gray;
+            Children[Current].Color = Color.White;
+            prevState = keyboardState;
+            base.Update(gameTime);
+        }
+    }
+}
